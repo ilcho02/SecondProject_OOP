97be0e64aeb47235d556f23a8f31293446e16176
ilcho02 <iliyannikolov02@abv.bg>
2022-06-10 15:25:42 +0300

    added String class partly

---
+++ ProjectString.cpp
@@ -0,0 +1,62 @@
+#include"ProjectString.h"
+#include<cstring>
+
+String::String() {
+    str = nullptr;
+    size = 0;
+}
+
+String::String(const String& other) {
+    int len = strlen(other.str) + 1;
+    str = new char[len];
+    strcpy_s(str, len, other.str);
+    size = other.size;
+}
+String::String(String&& other) {
+    size=other.size;
+    str = other.str;    
+    other.str = nullptr;
+}
+String& String::operator=(const String& other) {
+    if (this != &other) {
+        delete[] str;
+        size = other.size;
+        str = new char[size];
+        strcpy_s(str, size, other.str);
+    }
+    return *this;
+}
+String& String::operator=(String&& other) {
+    if (this != &other) {
+        delete[] str;
+        size = other.size;
+        str = other.str;
+        str = nullptr;
+    }
+    return *this;
+}
+String:: ~String() {
+    delete[] str;
+}
+String::String(const char* name) {
+    size = strlen(name) + 1;
+    str = new char[size];
+    strcpy_s(str, size, name);
+}
+size_t String::getSize()const {
+    return this->size;
+}
+const char* String::getStr()const {
+    return str;
+}
+
+
+
+
+ostream& operator<<(ostream& os, const String& src) {
+    os << src.str;
+    return os;
+}
+bool operator==(const String& l, const String& r) {
+    return (strcmp(l.getStr(), r.getStr()) == 0);
+}
---
+++ ProjectString.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class String {
+private:
+    char* str;
+    size_t size;
+public:
+    String();
+    String(const String&);
+    String(String&&);
+    String& operator=(const String&);
+    String& operator=(String&&);
+    ~String();
+    String(const char*);
+    size_t getSize()const;
+    const char* getStr()const;
+    
+
+    friend ostream& operator<<(ostream&, const String&);
+};
+
+bool operator==(const String&, const String&);
