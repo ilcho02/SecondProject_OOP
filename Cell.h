5c95fd4e7a06facf0dc7da7000d96e33b1caee5b
ilcho02 <iliyannikolov02@abv.bg>
2022-06-20 00:01:12 +0300

    added constructors

--- Cell.h
+++ Cell.h
@@ -2,20 +2,21 @@
 #include"ProjectString.h"
 #include"Pair.hpp"
 
-enum CellInfo{
+/*enum CellInfo{
     INT=1,
     DOUBLE,
     CHAR,
     FORMULA
-};
+};*/
 
 class Cell{
 private:
     String info;
     Pair<size_t,size_t> rowColumn;
-    CellInfo type;
-    public:
-    
+    unsigned int type;
+public:
+    Cell()=default;
+    Cell(const char*,size_t,size_t,unsigned int);
 
 
 
